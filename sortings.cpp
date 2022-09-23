f05476a1e0e5f4423fd31c32878f57df78cf95e6
Temo4ka <67861483+Temo4ka@users.noreply.github.com>
2022-09-23 20:51:52 +0300

    Update sortings.cpp

--- sortings.cpp
+++ sortings.cpp
@@ -3,13 +3,14 @@
 
 //const char *WHITELIST = "ABCDEFGHIJKLMNOPQRSUVWXYZabcdefghijklnopqrstuvwxyz";
 
-size_t partition(void *arrayOfStrings, size_t sizeOfElement, size_t leftParam, size_t rightParam, int (*cmp)(const void *aParam, const void *bParam));
+static size_t partition(void *arrayOfStrings, size_t sizeOfElement, size_t leftParam, size_t rightParam, int (*cmp)(const void *aParam, const void *bParam));
 
 void quickSort(void *arrayOfStrings, size_t sizeOfElement, size_t left, size_t right, int (*cmp)(const void *aParam, const void *bParam)) {
     if (right <= left )
         return;
 
     assert(arrayOfStrings != nullptr && "An Error in quickSort!");
+    assert(cmp            != nullptr && "An Error in quickSort!");
 
     size_t mid = partition(arrayOfStrings, sizeOfElement, left, right, cmp);
 
@@ -19,6 +20,7 @@ void quickSort(void *arrayOfStrings, size_t sizeOfElement, size_t left, size_t r
 
 size_t partition(void *arrayOfStrings, size_t sizeOfElement, size_t leftParam, size_t rightParam, int (*cmp)(const void *aParam, const void *bParam)) {
     assert(arrayOfStrings != nullptr && "An Error in Partition!\n");
+    assert(cmp != nullptr && "An Error in Partition!\n");
 
     size_t mid = rand() % (rightParam - leftParam) + leftParam;
     void *controlElement = ((char*) arrayOfStrings) + mid * sizeOfElement;
@@ -48,73 +50,6 @@ size_t partition(void *arrayOfStrings, size_t sizeOfElement, size_t leftParam, s
     }
 
     return rightParam;
-}
-
-void mergeSort(Lines *arrayOfStrings, size_t left, size_t right, int (*cmp)(const void *aParam, const void *bParam)) {
-    if (right - left == 1)
-        return;
-
-    size_t mid = (right + left) / 2;
-    mergeSort(arrayOfStrings, left, mid, cmp);
-    mergeSort(arrayOfStrings, mid, right, cmp);
-
-    size_t firstIndex    = left;
-    size_t secondIndex   = mid;
-    size_t passedSymbols = 0;
-    Lines resultOfSorting = {};
-    linesConstructor(&resultOfSorting, right - left);
-
-    while (passedSymbols < right - left) {
-        if (firstIndex == mid) {
-            resultOfSorting.array[passedSymbols] = arrayOfStrings->array[secondIndex];
-            ++secondIndex;
-        } else if (secondIndex == right) {
-            resultOfSorting.array[passedSymbols] = arrayOfStrings->array[firstIndex];
-            ++firstIndex;
-        } else if (cmp(&arrayOfStrings->array[firstIndex], &arrayOfStrings->array[secondIndex])) {
-            resultOfSorting.array[passedSymbols] = arrayOfStrings->array[firstIndex];
-            ++firstIndex;
-        } else {
-            resultOfSorting.array[passedSymbols] = arrayOfStrings -> array[secondIndex];
-            ++secondIndex;
-        }
-        ++passedSymbols;
-    }
-
-    for (size_t resultIndex = 0, arrayIndex = left; resultIndex < right - left; ++resultIndex, ++arrayIndex)
-        arrayOfStrings->array[arrayIndex] = resultOfSorting.array[resultIndex];
-}
-
-int comparatorForMergeSort(const void *aParam, const void *bParam) {
-    assert(aParam != nullptr && bParam != nullptr && "An Error in comparatorForMergeSort!!");
-
-    const Line *a = (const Line*) aParam;
-    const Line *b = (const Line*) bParam;
-
-    if (a->line == nullptr)
-        return true;
-    if (b->line == nullptr)
-        return false;
-
-    size_t index1 = 0;
-    size_t index2 = 0;
-    while (a->line[index1] != '\0' || b->line[index2] != '\0') {
-
-        while (a->line[index1] != '\0' && !isalpha(a->line[index1]))
-            ++index1;
-        while (a->line[index2] != '\0' && !isalpha(b->line[index2]))
-            ++index2;
-
-        if (a->line[index1] < b->line[index2])
-            return true;
-        if (a->line[index1] > b->line[index2])
-            return false;
-
-        ++index1;
-        ++index2;
-    }
-
-    return false;
 }
 
 int comparatorForQsort(const void *aParam, const void *bParam) {
@@ -147,6 +82,15 @@ int comparatorForQsort(const void *aParam, const void *bParam) {
     }
 
     return Equal;
+}
+
+int originComparator(const void *aParam, const void *bParam) {
+    assert(aParam != nullptr && bParam != nullptr && "An Error in originComparator!!!");
+
+    const Line *a = (const Line*) aParam;
+    const Line *b = (const Line*) bParam;
+
+    return a -> line - b -> line;
 }
 
 int revComparator(const void *aParam, const void *bParam) {
